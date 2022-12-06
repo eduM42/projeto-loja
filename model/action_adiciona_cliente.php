500832064e04ae12aa17b0cfff026299d938c790
eduM42 <107020621+eduM42@users.noreply.github.com>
2022-12-06 00:09:23 -0300

    a

--- model/action_adiciona_cliente.php
+++ model/action_adiciona_cliente.php
@@ -8,13 +8,13 @@
       $data = filter_input(INPUT_POST, 'txtdatanasc');
     //  $data= '2005-12-26';
 
-      $sql = $conecta->prepare("INSERT into tab_clientes (cli_nome, cli_sobrenome, cli_fone, cli_data_nasc) VALUES (:nome,:sobrenome,:fone,:dt)");
+      $sql = $conecta->prepare("INSERT into tab_clientes (cli_nome, cli_sobrenome, cli_fone, cli_data_nasc) VALUES (:nome,:sobrenome,:fone,:dt_nasc)");
       $sql->bindValue(':nome',$nome);
       $sql->bindValue(':sobrenome',$sobrenome);
       $sql->bindValue(':fone', $fone);
-      $sql->bindValue(':dt', $data);
+      $sql->bindValue(':dt_nasc', $data);
       $sql->execute();
-      header('Location:listagem_clientes.php');
+      header('Location: listagem_clientes.php');
    }
    echo "$nome $sobrenome $data $fone";
 
