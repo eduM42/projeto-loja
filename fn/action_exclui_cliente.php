d172b5c6d2527112357ffd136e048d5ecba8d5eb
eduM42 <107020621+eduM42@users.noreply.github.com>
2022-12-11 18:32:14 -0300

    a

--- fn/action_exclui_cliente.php
+++ fn/action_exclui_cliente.php
@@ -3,6 +3,9 @@
     require_once 'connect.php';
 
     $codigo=$_GET['id'];
+    $exclui=$conecta->prepare('DELETE FROM tab_carrinho WHERE cli_id=:codigo');
+    $exclui->bindValue(':codigo', $codigo);
+    $exclui->execute();
 
     $exclui=$conecta->prepare('DELETE FROM tab_clientes WHERE cli_id=:codigo');
     $exclui->bindValue(':codigo', $codigo);
